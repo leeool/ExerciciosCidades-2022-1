0619a91fa5bb38085faeb7e1df10587b163f5a85
anoddityleol <lgonsalez02@gmail.com>
2022-03-04 13:09:32 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ubatuba");
+Console.WriteLine("Caraguatatuba");
+Console.WriteLine("Ilhabela");
+Console.WriteLine("São Sebastião");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Iguape");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Cananéia");
